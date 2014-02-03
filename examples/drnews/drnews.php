112f4b3ed48322eeea6bf80e5d9a8cb1b728acbe
Thomas Horsten <thomas@horsten.com>
2014-02-03 02:26:18 +0000

    DR updates

--- examples/drnews/drnews.php
+++ examples/drnews/drnews.php
@@ -57,86 +57,107 @@ function main()
 	$doc = file_get_contents($url);
 
 	$upddate = dkzdate(time());
-	
+
 	$allnews = "\n\nNyheder fra dr.dk opdateret $upddate =\n\n\n";
 	$articles = array();
 	//echo $allnews;
 	//exit(0);
 
 	$DOM = new DOMDocument;
+	libxml_use_internal_errors(true);
 	$DOM->loadHTML($doc);
+	libxml_use_internal_errors(false);
 
-	//get all H1
-	$items = $DOM->getElementsByTagName('div');
-
-	//echo "Encoding: ".$DOM->encoding."\n";
-
-// display all H1 text
-	foreach ($items as $item) {
-		//echo $item->getAttribute('id') . "\n";
-		if (!strcasecmp($item->getAttribute('class'), "txtContent")) {
-			//echo "Content node found\n";
-			$h = ($item->getElementsByTagName('h1'));
-			if ($h->length != 1) {
-				//echo "Invalid length ".$h->length."\n";
-				continue;
-			}
-			$headline = trim($h->item(0)->nodeValue);
-			$d = $item->getElementsByTagName('span');
-			if (($d->length != 1) || $d->item(0)->getAttribute('class') != "stamp") {
-				echo "Invalid date stamp\n";
-				continue;
-			}
-			$datestamp = stddate(trim($d->item(0)->nodeValue));
-			//echo "Good story: " . $datestamp . " - " . $headline."\n";
-
-			// Replace <BR>'s with newline
-			$brs = $item->getElementsByTagName('br');
-			foreach ($brs as $node) {
-				$node->parentNode->replaceChild($DOM->createTextNode("\n\n"), $node);
-			}
-
-			$brs = $item->getElementsByTagName('strong');
-			foreach ($brs as $node) {
-				$node->parentNode->replaceChild($DOM->createTextNode("= ".trim($node->nodeValue)." =\n\n"), $node);
-			}
-		
-			// Replace paragraphs with double newline
-			$content = "";
-			$ps = $item->getElementsByTagName('p');
-			foreach ($ps as $node) {
-
-				// Replace <STRONG>'s with  "= xxx =\n\n" (subheadings use this)
-				$brs = $node->getElementsByTagName('strong');
-				foreach ($brs as $node1) {
-					$node1->parentNode->replaceChild($DOM->createTextNode("= ".trim($node1->nodeValue)." =\n\n"), $node1);
-				}
-
-				$p = trim($node->nodeValue);
-				if (preg_match('|LÆS OGSÅ:.*|', $p))
-					continue;
-				$content .= $p . "\n\n";
-			}
-
-			$content = str_replace(" ", " ", $content); /* &nbsp; */
-			$content = str_replace("_", " ", $content);
-			$content = preg_replace('|^\s+$|m', '', $content);
-			$content = str_replace("\r", "", $content);
-			$content = str_replace("\n\n\n", "\n\n", $content);
-			$content = str_replace("\n\n\n", "\n\n", $content);
-			$content = trim(wordwrap($content, 75));
-			//echo "Content:\n" . $content. "\n";
-			//break;
-
-			$articles[] = array(
-				'datestamp'  => $datestamp,
-				'headline'   => $headline,
-				'content'    => "*\n\n" . $headline . " =\n\n" . $datestamp . " =\n\n" . $content . "\n\n+\n\n\n");
-
-			$allnews .= "*\n\n" . $headline . " =\n\n" . $datestamp . " =\n\n" . $content . "\n\n+\n\n\n";
-		}
-
+	// Get the correct section, all "last 24 hours" articles
+	// are <h3>'s inside a <section class="dr-list">
+	$domxpath = new DOMXPath($DOM);
+	$res = $domxpath->query("//section[@class='dr-list']");
+	print_r($res);
+	if ($res->length != 1) {
+		fprintf(STDERR, "Unable to find correct section\n");
+		exit(1);
 	}
+	$section = $res->item(0);
+	//print_r($section);
+	//exit(0);
+
+	//get all articles
+	$items = array();
+	$articles = $section->getElementsByTagName('article');
+	foreach ($articles as $article) {
+		$h = $article->getElementsByTagName('h3');
+		if ($h->length != 1) {
+			echo "Invalid h3 count ".$h->length."\n";
+			continue;
+		}
+		$h = $h->item(0);
+		$headline = trim($h->nodeValue);
+		$a = $h->getElementsByTagName('a');
+		if ($a->length != 1) {
+			echo "Invalid a count ".$h->length."\n";
+			continue;
+		}
+		$a = $a->item(0);
+		$storyurl = $a->attributes->getNamedItem('href')->nodeValue;
+		if (preg_match('|^/Sporten/|', $storyurl))
+			continue;
+		echo "u: $storyurl h: " . $headline . "\n";
+		continue;
+		
+		$d = $item->getElementsByTagName('span');
+		if (($d->length != 1) || $d->item(0)->getAttribute('class') != "stamp") {
+			echo "Invalid date stamp\n";
+			continue;
+		}
+		$datestamp = stddate(trim($d->item(0)->nodeValue));
+		//echo "Good story: " . $datestamp . " - " . $headline."\n";
+
+		// Replace <BR>'s with newline
+		$brs = $item->getElementsByTagName('br');
+		foreach ($brs as $node) {
+			$node->parentNode->replaceChild($DOM->createTextNode("\n\n"), $node);
+		}
+		
+		$brs = $item->getElementsByTagName('strong');
+		foreach ($brs as $node) {
+			$node->parentNode->replaceChild($DOM->createTextNode("= ".trim($node->nodeValue)." =\n\n"), $node);
+		}
+		
+		// Replace paragraphs with double newline
+		$content = "";
+		$ps = $item->getElementsByTagName('p');
+		foreach ($ps as $node) {
+
+			// Replace <STRONG>'s with  "= xxx =\n\n" (subheadings use this)
+			$brs = $node->getElementsByTagName('strong');
+			foreach ($brs as $node1) {
+				$node1->parentNode->replaceChild($DOM->createTextNode("= ".trim($node1->nodeValue)." =\n\n"), $node1);
+			}
+			
+			$p = trim($node->nodeValue);
+			if (preg_match('|LÆS OGSÅ:.*|', $p))
+				continue;
+			$content .= $p . "\n\n";
+		}
+		
+		$content = str_replace(" ", " ", $content); /* &nbsp; */
+		$content = str_replace("_", " ", $content);
+		$content = preg_replace('|^\s+$|m', '', $content);
+		$content = str_replace("\r", "", $content);
+		$content = str_replace("\n\n\n", "\n\n", $content);
+		$content = str_replace("\n\n\n", "\n\n", $content);
+		$content = trim(wordwrap($content, 75));
+		//echo "Content:\n" . $content. "\n";
+		//break;
+
+		$articles[] = array(
+			'datestamp'  => $datestamp,
+			'headline'   => $headline,
+			'content'    => "*\n\n" . $headline . " =\n\n" . $datestamp . " =\n\n" . $content . "\n\n+\n\n\n");
+		
+		$allnews .= "*\n\n" . $headline . " =\n\n" . $datestamp . " =\n\n" . $content . "\n\n+\n\n\n";
+	}
+	
 	$allnews .= "\n%\n\n\n";
 	file_put_contents("drnews.txt", $allnews);
 	$shellscript = "";
